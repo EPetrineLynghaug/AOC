c35cfe8ac322dd9cc0bd984ad74c4caa957541a7
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2023-11-11 15:22:07 +0100

    Added day template

---
+++ 2015/days/day-mal/src/main.rs
@@ -0,0 +1,43 @@
+use colored::Colorize;
+
+mod part1;
+mod part2;
+
+fn main() {
+    /*Den faktiske koden*/
+    let input = include_str!("../input.txt");
+
+    /*Part one solution */
+    // println!("Part 1: {}", part1::solution(input)).green();
+    println!(
+        "{} {}",
+        format!("Part 1:").underline(),
+        format!("{}", part1::solution(input)).bold()
+    );
+
+    /*Part two solution */
+    println!(
+        "{} {}",
+        format!("Part 2:").underline(),
+        format!("{}", part2::solution(input)).bold()
+    );
+}
+#[cfg(test)] /*cft bare kompiler når. denne kan bare kjøres om du skriver cargo test.  */
+
+mod test {
+    use super::*; /*denne  sier at denne er helt unik og ikke har noe med resten av programmet å gjøre. denne må være her for ellers kan den tro du stjeler eller hacker. */
+
+    const INPUT: &str = ""; /*str står for sting. LUFTIG SPRÅK SOM TRENGER LUFT.*/
+
+    #[test]
+    fn part1_works() {
+        let resulte = part1::solution(INPUT);
+        assert_eq!(resulte, todo!());
+    }
+
+    #[test]
+    fn part2_works() {
+        let resulte = part2::solution(INPUT);
+        assert_eq!(resulte, todo!());
+    }
+}
